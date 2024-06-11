fb2c2116788fc886cfc2e956617362115b3880be
chalitha chamod <chalithachamod3031@gmail.com>
2024-06-11 21:33:14 +0530

    new databse , doctor design

--- assets/database/healthcare.sql
+++ assets/database/healthcare.sql
@@ -19,6 +19,20 @@
 CREATE DATABASE IF NOT EXISTS `healthcare` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `healthcare`;
 
+-- Dumping structure for table healthcare.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `mobile` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table healthcare.admin: ~1 rows (approximately)
+INSERT INTO `admin` (`id`, `name`, `password`, `mobile`, `email`) VALUES
+	(1, 'admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '0716589563', 'admin@gmail.com');
+
 -- Dumping structure for table healthcare.patients_details
 CREATE TABLE IF NOT EXISTS `patients_details` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -36,9 +50,9 @@ CREATE TABLE IF NOT EXISTS `patients_details` (
   CONSTRAINT `FK_patients_details_register_patients` FOREIGN KEY (`patients_id`) REFERENCES `registered_patients` (`p_id`),
   CONSTRAINT `FK_patients_details_register_reception` FOREIGN KEY (`reception_id`) REFERENCES `registered_reception` (`id_num`),
   CONSTRAINT `FK_patients_details_registered_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `registered_doctor` (`id_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table healthcare.patients_details: ~6 rows (approximately)
+-- Dumping data for table healthcare.patients_details: ~14 rows (approximately)
 INSERT INTO `patients_details` (`id`, `patients_id`, `reception_id`, `symptoms`, `Prescriptions`, `doctor_id`, `age`, `symptoms_date`) VALUES
 	(1, 5, '1542658v', 'fdfsf', 'sdsdsdasda', '785645v', 25, '2024-06-05 22:00:24'),
 	(2, 6, '1542658v', 'fdf', '', '785645v', 35, '2024-06-05 22:00:24'),
@@ -52,7 +66,8 @@ INSERT INTO `patients_details` (`id`, `patients_id`, `reception_id`, `symptoms`,
 	(10, 2, '1542658v', 'aaaaaaaaaaaaaaaaaaaaa', '', '785645v', 30, '2024-06-07 21:24:05'),
 	(11, 10, '1542658v', 'fdjsp kjdpfmk[p kfkfls ', '', '785645v', 5, '2024-06-07 21:25:07'),
 	(12, 10, '1542658v', 'gklgl pgl [[ ]. ggfg', '', '785645v', 16, '2024-06-07 21:25:58'),
-	(13, 10, '1542658v', 'a', '', '785645v', 20, '2024-06-07 21:27:02');
+	(13, 10, '1542658v', 'a', '', '785645v', 20, '2024-06-07 21:27:02'),
+	(14, 11, '5468421v', 'abc\ndef', '', '785645v', 60, '2024-06-11 21:21:18');
 
 -- Dumping structure for table healthcare.registered_doctor
 CREATE TABLE IF NOT EXISTS `registered_doctor` (
@@ -63,12 +78,43 @@ CREATE TABLE IF NOT EXISTS `registered_doctor` (
   `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `verification_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `register_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_num`) USING BTREE
+  `status` int DEFAULT '1',
+  `age` int DEFAULT NULL,
+  `address` text,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_num`) USING BTREE,
+  KEY `FK_registered_doctor_ststus_type` (`status`),
+  CONSTRAINT `FK_registered_doctor_ststus_type` FOREIGN KEY (`status`) REFERENCES `status_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table healthcare.registered_doctor: ~0 rows (approximately)
-INSERT INTO `registered_doctor` (`id_num`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`) VALUES
-	('785645v', 'doc01', 'doc@gmail.com', '0716585698', '12342', NULL, '2024-06-05 17:32:06');
+-- Dumping data for table healthcare.registered_doctor: ~4 rows (approximately)
+INSERT INTO `registered_doctor` (`id_num`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`, `status`, `age`, `address`, `image`) VALUES
+	('123456789', NULL, 'cchamod93@gmail.com', NULL, '20edb0ec04f4055af8e5895934d598a54a370dab0f38882e2a8eee8247584587', NULL, NULL, 1, NULL, NULL, NULL),
+	('196971900830', NULL, 'chamodnadeeshan91@gmail.com', NULL, '6666a437', NULL, NULL, 1, NULL, NULL, NULL),
+	('20912658453V', NULL, 'kawshalya20001025@gmail.com', NULL, '6667f99f', NULL, NULL, 1, NULL, NULL, NULL),
+	('785645v', 'doc01', 'doc@gmail.com', '0716585698', '12342', NULL, '2024-06-05 17:32:06', 1, NULL, NULL, NULL);
+
+-- Dumping structure for table healthcare.registered_mlt
+CREATE TABLE IF NOT EXISTS `registered_mlt` (
+  `id_num` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mobile` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `verification_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `register_date` datetime DEFAULT NULL,
+  `status` int DEFAULT '1',
+  `age` int DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_num`) USING BTREE,
+  KEY `FK_mlt_status_type` (`status`) USING BTREE,
+  CONSTRAINT `FK_mlt_status_type` FOREIGN KEY (`status`) REFERENCES `status_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table healthcare.registered_mlt: ~1 rows (approximately)
+INSERT INTO `registered_mlt` (`id_num`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`, `status`, `age`, `address`, `image`) VALUES
+	('196971900830', NULL, 'chalithachamod3031@gmail.com', NULL, '701b4f3b4725af5f7e2e9bd66bd18d487108d870283d2b758395606b4a140c7d', NULL, NULL, 1, NULL, NULL, NULL);
 
 -- Dumping structure for table healthcare.registered_patients
 CREATE TABLE IF NOT EXISTS `registered_patients` (
@@ -84,9 +130,9 @@ CREATE TABLE IF NOT EXISTS `registered_patients` (
   PRIMARY KEY (`p_id`),
   KEY `FK_register_patients_register_reception` (`reception_id`),
   CONSTRAINT `FK_register_patients_register_reception` FOREIGN KEY (`reception_id`) REFERENCES `registered_reception` (`id_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table healthcare.registered_patients: ~8 rows (approximately)
+-- Dumping data for table healthcare.registered_patients: ~10 rows (approximately)
 INSERT INTO `registered_patients` (`p_id`, `name`, `mobile`, `email`, `id_num`, `address`, `age`, `register_date`, `reception_id`) VALUES
 	(2, 'abcd efg', '0713772006', 'chalithachamod3031@gmail.com', '196971900830', 'pinnagollawththa maharachchimulla alawwa', 30, '2024-06-07 21:24:00', '1542658v'),
 	(3, 'chalitha chamod', '0713772006', 'chalithachamod3031@gmail.com', '196971900830', 'pinnagollawththa maharachchimulla alawwa', 12, '2024-06-05 17:55:27', '1542658v'),
@@ -96,23 +142,68 @@ INSERT INTO `registered_patients` (`p_id`, `name`, `mobile`, `email`, `id_num`, 
 	(7, 'sanuka tharaka', '0711234567', 'sanuka@gmail.com', '254685v', 'galgamuwa alawwa', 30, '2024-06-07 12:26:29', '1542658v'),
 	(8, 'sanuka tharaka', '07165846895', 'sanuka@gmail.com', '254685v', 'galgamuwa alawwa', 26, '2024-06-05 22:02:06', '1542658v'),
 	(9, 'chalitha chamod', '0783772006', 'chalithachamod@gmail.com', '4564', 'maharachchimulla alawwa', 20, '2024-06-07 12:24:53', '1542658v'),
-	(10, 'adeesha', '07165984679', 'abcdeg@gmail.com', '24698752', 'alawwa kurunegala', 20, '2024-06-07 21:26:59', '1542658v');
+	(10, 'adeesha', '07165984679', 'abcdeg@gmail.com', '24698752', 'alawwa kurunegala', 20, '2024-06-07 21:26:59', '1542658v'),
+	(11, 'new pation ', '0716584569', 'abc@gmail.com', '196971900830', 'alawwa', 60, '2024-06-11 21:21:18', '5468421v');
+
+-- Dumping structure for table healthcare.registered_pharmacists
+CREATE TABLE IF NOT EXISTS `registered_pharmacists` (
+  `id_num` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mobile` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `verification_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `register_date` datetime DEFAULT NULL,
+  `status` int DEFAULT '1',
+  `age` int DEFAULT NULL,
+  `address` text,
+  `image` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_num`) USING BTREE,
+  KEY `FK_registered_pharmacists_status_type` (`status`),
+  CONSTRAINT `FK_registered_pharmacists_status_type` FOREIGN KEY (`status`) REFERENCES `status_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table healthcare.registered_pharmacists: ~1 rows (approximately)
+INSERT INTO `registered_pharmacists` (`id_num`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`, `status`, `age`, `address`, `image`) VALUES
+	('196971900830', NULL, 'chamodnadeeshan91@gmail.com', NULL, '6667f8fc', NULL, NULL, 1, NULL, NULL, NULL);
 
 -- Dumping structure for table healthcare.registered_reception
 CREATE TABLE IF NOT EXISTS `registered_reception` (
   `id_num` varchar(50) NOT NULL,
+  `address` text,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `mobile` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `password` varchar(200) DEFAULT NULL,
   `verification_code` varchar(50) DEFAULT NULL,
   `register_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_num`)
+  `image` varchar(100) DEFAULT NULL,
+  `status` int DEFAULT '1',
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`id_num`),
+  KEY `FK_registered_reception_status_type` (`status`),
+  CONSTRAINT `FK_registered_reception_status_type` FOREIGN KEY (`status`) REFERENCES `status_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table healthcare.registered_reception: ~0 rows (approximately)
-INSERT INTO `registered_reception` (`id_num`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`) VALUES
-	('1542658v', 'recip_01', 'res@gmail.com', '0716584569', '12345', NULL, '2024-06-05 17:29:18');
+-- Dumping data for table healthcare.registered_reception: ~4 rows (approximately)
+INSERT INTO `registered_reception` (`id_num`, `address`, `name`, `email`, `mobile`, `password`, `verification_code`, `register_date`, `image`, `status`, `age`) VALUES
+	('123456789V', NULL, NULL, 'cchamod93@gmail.com', NULL, '1a0726492bcbbfbd90dedf100497e9414cc1ed47ddad33cb60bcbbc9392883a6', NULL, NULL, NULL, 1, NULL),
+	('1542658v', NULL, 'recip_01', 'res@gmail.com', '0716584569', '12345', NULL, '2024-06-05 17:29:18', NULL, NULL, NULL),
+	('156879245v', 'alawwa', 'tharaka', 'cchamod93@gmail.com', '0754568963', '8da41ee37bfc4a6afa809c4f9e59730fc2e45ca00fe7f3e2bd7d1b41b62184cb', NULL, '2024-06-11 21:06:56', '66686bfba9ab8.jpeg', 1, 55),
+	('196971900830', 'aaa bbb ccc', 'sanuka', 'chalithachamod3031@gmail.com', '0713658698', 'd954ae72c98a3fbab67e1dc849bb2c076196740a901d33f46b930a59d421d27f', NULL, '2024-06-11 20:40:42', '6668688ec15c2.jpeg', 1, 35),
+	('5468421v', 'kurunegala', 'new rec', 'chamodnadeeshan91@gmail.com', '078456985', 'c0a779ff6d98ada508205607cae551d84b8273de27aa1cea77a093ccd18295e1', NULL, '2024-06-11 21:18:26', '666871ca0649b.jpeg', 1, 36);
+
+-- Dumping structure for table healthcare.status_type
+CREATE TABLE IF NOT EXISTS `status_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table healthcare.status_type: ~2 rows (approximately)
+INSERT INTO `status_type` (`id`, `name`) VALUES
+	(1, 'Allow'),
+	(2, 'block');
 
 -- Dumping structure for table healthcare.user
 CREATE TABLE IF NOT EXISTS `user` (
---
+++ doctor/doctor.js
@@ -0,0 +1,59 @@
+const root = document.documentElement;
+
+function getCustomPropertyValue(propertyName) {
+    return getComputedStyle(root).getPropertyValue(propertyName).trim();
+}
+
+let basecolor = getCustomPropertyValue('--base-color');
+let mediumgrey = getCustomPropertyValue('--medium-gray');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    const content = document.getElementById('content');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function(e) {
+            tabs.forEach(colorTab =>{
+                colorTab.style.backgroundColor = mediumgrey;
+            })
+            e.preventDefault();
+            const target = this.getAttribute('data-target');
+            console.log(target)
+            fetch(target)
+            .then(response => response.text())
+            .then(data => {
+                content.innerHTML = data;
+                tab.style.backgroundColor = basecolor;
+                })
+                .catch(error => {
+                    console.error('Error loading content:', error);
+                });
+        });
+    });
+});
+
+
+function filterTable() {
+    const searchBar = document.getElementById('searchBar');
+    const filter = searchBar.value.toLowerCase();
+    const table = document.getElementById('dataTable');
+    const rows = table.getElementsByTagName('tr');
+
+    for (let i = 1; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('td');
+        let rowText = '';
+        for (let j = 0; j < cells.length; j++) {
+            rowText += cells[j].textContent || cells[j].innerText;
+        }
+        if (rowText.toLowerCase().indexOf(filter) > -1) {
+            rows[i].style.display = '';
+        } else {
+            rows[i].style.display = 'none';
+        }
+    }
+}
+
+
+function examinePatient() {
+    window.location.href = 'examine.html';
+}
