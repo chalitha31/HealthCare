842f83aa1d0430eb3fa8c3f33fcbf0f5f4cd15c8
chalitha chamod <chalithachamod3031@gmail.com>
2024-07-07 15:03:14 +0530

    new Database

--- assets/database/healthcare.sql
+++ assets/database/healthcare.sql
@@ -47,9 +47,9 @@ CREATE TABLE IF NOT EXISTS `bloodtest` (
   PRIMARY KEY (`id`),
   KEY `FK_bloodtest_patients_details` (`patientDetails_id`),
   CONSTRAINT `FK_bloodtest_patients_details` FOREIGN KEY (`patientDetails_id`) REFERENCES `patients_details` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table healthcare.bloodtest: ~12 rows (approximately)
+-- Dumping data for table healthcare.bloodtest: ~17 rows (approximately)
 INSERT INTO `bloodtest` (`id`, `patientDetails_id`, `reportName`, `test_type`, `issued_Date`, `mlt_id`) VALUES
 	(1, 1, 'lipid_medical_report_1_K.R.karunadasa_2024-07-04_13-01-21.jpg', 'lipid', '2024-07-04 13:01:21', '1970671234'),
 	(2, 7, 'hdl_medical_report_7_karunawathimenike_2024-06-29_02-16-33.jpg', 'hdl', '2024-06-29 02:16:33', '1970671234'),
@@ -61,10 +61,13 @@ INSERT INTO `bloodtest` (`id`, `patientDetails_id`, `reportName`, `test_type`, `
 	(8, 12, 'ldl_medical_report_12_M.R.Rathnasiri_2024-07-04_18-09-26.jpg', 'ldl', '2024-07-04 18:09:26', '1970671234'),
 	(9, 14, 'ppbs_medical_report_14_NipunAnuoama_2024-06-30_15-46-40.jpg', 'ppbs', '2024-06-30 15:46:40', '1970671234'),
 	(10, 14, 'cbc_medical_report_14_NipunAnuoama_2024-06-30_15-47-50.jpg', 'cbc', '2024-06-30 15:47:50', '1970671234'),
-	(11, 7, NULL, 'lipid', NULL, NULL),
-	(12, 13, NULL, 'ppbs', NULL, NULL),
+	(11, 7, 'lipid_medical_report_7_karunawathimenike_2024-07-06_14-45-49.jpg', 'lipid', '2024-07-06 14:45:49', '1970671234'),
+	(12, 13, 'ppbs_medical_report_13_Damayanthikumari_2024-07-06_14-45-41.jpg', 'ppbs', '2024-07-06 14:45:41', '1970671234'),
 	(13, 15, 'cbc_medical_report_15_sithuminaveesha_2024-07-04_18-11-55.jpg', 'fbc', '2024-07-04 18:11:55', '1970671234'),
-	(14, 15, 'fpg_medical_report_15_sithuminaveesha_2024-07-04_18-13-42.jpg', 'fpg', '2024-07-04 18:13:42', '1970671234');
+	(14, 15, 'fpg_medical_report_15_sithuminaveesha_2024-07-04_18-13-42.jpg', 'fpg', '2024-07-04 18:13:42', '1970671234'),
+	(15, 7, 'fpg_medical_report_7_karunawathimenike_2024-07-06_14-47-34.jpg', 'fpg', '2024-07-06 14:47:34', '1970671234'),
+	(16, 13, 'ppbs_medical_report_13_Damayanthikumari_2024-07-06_14-50-02.jpg', 'ppbs', '2024-07-06 14:50:02', '1970671234'),
+	(17, 7, 'lipid_medical_report_7_karunawathimenike_2024-07-06_14-50-30.jpg', 'lipid', '2024-07-06 14:50:30', '1970671234');
 
 -- Dumping structure for table healthcare.medicines
 CREATE TABLE IF NOT EXISTS `medicines` (
@@ -76,16 +79,16 @@ CREATE TABLE IF NOT EXISTS `medicines` (
   `purchase_date` date DEFAULT NULL,
   `outofstock_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table healthcare.medicines: ~15 rows (approximately)
+-- Dumping data for table healthcare.medicines: ~18 rows (approximately)
 INSERT INTO `medicines` (`id`, `name`, `brand`, `quantity`, `exp`, `purchase_date`, `outofstock_date`) VALUES
 	(1, 'Zithrocin', 'Zetra pvt ltd', 9900, '2025-02-28', '2024-04-03', NULL),
 	(2, 'Paracetamol', 'ABC Pharma', 256, '2024-10-24', '2024-02-03', NULL),
 	(3, 'Amoxicillin', 'Amox pvt', 439, '2024-03-18', '2024-07-01', NULL),
 	(4, 'Aspirin', 'Aspirin', 3537, '2026-07-03', '2024-06-03', NULL),
 	(5, 'zithrocin', 'Zetra pvt ltd', 0, '2024-07-06', '2024-06-03', '2024-07-03'),
-	(6, 'pirition', 'zetra pvt ltd', 460, '2024-08-28', '2024-07-03', NULL),
+	(6, 'pirition', 'zetra pvt ltd', 459, '2024-08-28', '2024-07-03', NULL),
 	(7, 'Acetaminophen', 'Ace pvt', 0, '2025-06-23', '2024-05-03', '2024-07-04'),
 	(8, 'Calcium', 'ABC PVT LTD', 0, '2024-07-24', '2022-07-03', '2024-07-04'),
 	(9, 'Dopamine', 'Dop Company', 4, '2024-04-07', '2024-06-17', NULL),
@@ -94,7 +97,10 @@ INSERT INTO `medicines` (`id`, `name`, `brand`, `quantity`, `exp`, `purchase_dat
 	(12, 'zithrocin', 'Zetra pvt ltd', 0, '2024-09-07', '2023-07-03', NULL),
 	(13, 'gGhespirin', 'fsf', 40, '2024-08-28', '2024-03-03', NULL),
 	(14, 'Zithrocin', 'Zetra pvt ltd', 1560, '2026-02-03', '2024-07-03', NULL),
-	(15, 'abcde (200mg)', 'abc', 150, '2024-08-16', '2024-07-04', NULL);
+	(15, 'abcde (200mg)', 'abc', 150, '2024-08-16', '2024-07-04', NULL),
+	(16, 'A (50mg)', 'a', 118, '2024-08-24', '2024-07-05', NULL),
+	(17, 'A (50mg)', 'a', 35, '2024-08-28', '2024-07-06', NULL),
+	(18, 'gae (50mg)', 'ffe hwer', 120, '2024-07-20', '2024-07-06', NULL);
 
 -- Dumping structure for table healthcare.medicines_recode
 CREATE TABLE IF NOT EXISTS `medicines_recode` (
@@ -108,9 +114,9 @@ CREATE TABLE IF NOT EXISTS `medicines_recode` (
   KEY `FK__medicines` (`medicine_id`),
   CONSTRAINT `FK__medicines` FOREIGN KEY (`medicine_id`) REFERENCES `medicines` (`id`),
   CONSTRAINT `FK__patients_details` FOREIGN KEY (`patientDetails_id`) REFERENCES `patients_details` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table healthcare.medicines_recode: ~10 rows (approximately)
+-- Dumping data for table healthcare.medicines_recode: ~11 rows (approximately)
 INSERT INTO `medicines_recode` (`id`, `patientDetails_id`, `medicine_id`, `qty`, `date`) VALUES
 	(1, 2, 3, 30, '2024-06-29'),
 	(2, 11, 7, 50, '2024-06-30'),
@@ -121,27 +127,31 @@ INSERT INTO `medicines_recode` (`id`, `patientDetails_id`, `medicine_id`, `qty`,
 	(7, 14, 2, 50, '2024-06-30'),
 	(8, 14, 5, 14, '2024-06-30'),
 	(9, 6, 5, 5, '2024-07-03'),
-	(10, 6, 6, 10, '2024-07-03');
+	(10, 6, 6, 10, '2024-07-03'),
+	(11, 10, 6, 1, '2024-07-07');
 
 -- Dumping structure for table healthcare.mlt_equipments
 CREATE TABLE IF NOT EXISTS `mlt_equipments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
-  `weekly` int DEFAULT NULL,
+  `purchase_date` date DEFAULT NULL,
+  `avalable_quantity` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table healthcare.mlt_equipments: ~8 rows (approximately)
-INSERT INTO `mlt_equipments` (`id`, `name`, `quantity`, `weekly`) VALUES
-	(1, 'Blood collection tubes', 35, 21),
-	(2, 'Latex gloves', 135, 25),
-	(3, 'Urine collection cups', 10, 27),
-	(4, 'Sterile syringes', 481, 54),
-	(5, 'Hypodermic needles', 18, 16),
-	(6, 'EDTA tubes (lavender top)', -13, 21),
-	(7, 'Sterile gauze pads', 81, 41),
-	(8, 'test tube', 11, 9);
+-- Dumping data for table healthcare.mlt_equipments: ~10 rows (approximately)
+INSERT INTO `mlt_equipments` (`id`, `name`, `quantity`, `purchase_date`, `avalable_quantity`) VALUES
+	(1, 'Blood collection tubes', 35, NULL, 35),
+	(2, 'Latex gloves', 135, NULL, 0),
+	(3, 'Urine collection cups', 10, NULL, 10),
+	(4, 'Sterile syringes', 481, NULL, 250),
+	(5, 'Hypodermic needles', 18, NULL, 10),
+	(6, 'EDTA tubes (lavender top)', 13, NULL, 12),
+	(7, 'Sterile gauze pads', 81, NULL, 56),
+	(8, 'test tube', 11, NULL, 0),
+	(9, 'ad', 123, '2024-07-06', 120),
+	(10, 'abcedf` tube', 150, '2024-07-06', 150);
 
 -- Dumping structure for table healthcare.patients_details
 CREATE TABLE IF NOT EXISTS `patients_details` (
@@ -165,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `patients_details` (
   CONSTRAINT `FK_patients_details_registered_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `registered_doctor` (`id_num`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table healthcare.patients_details: ~15 rows (approximately)
+-- Dumping data for table healthcare.patients_details: ~16 rows (approximately)
 INSERT INTO `patients_details` (`id`, `patients_id`, `age`, `reception_id`, `symptoms`, `medical_report`, `Prescriptions`, `Prescriptions_date`, `doctor_id`, `symptoms_date`, `medicine_status`) VALUES
 	(1, 1, 68, '1990563467', ' High Blood Pressure\nOften asymptomatic, but can include headaches, shortness of breath, nosebleeds.\nGenetics, diet, stress, underlying conditions.', 'yes', '', NULL, '0000000000', '2024-06-28 17:39:38', 'pending'),
 	(2, 2, 64, '1990563467', 'Diabetes\nIncreased thirst, frequent urination, fatigue, blurred vision.\n\n', 'no', 'Insulin.\r\nempagliflozin', '2024-06-28 18:40:44', '1988235678', '2024-06-28 17:42:24', 'true'),
@@ -176,7 +186,7 @@ INSERT INTO `patients_details` (`id`, `patients_id`, `age`, `reception_id`, `sym
 	(7, 7, 68, '1990563467', 'Muscle Pain\nSoreness, stiffness, aching in muscles.', 'no', '', NULL, '0000000000', '2024-06-28 18:09:20', 'pending'),
 	(8, 8, 24, '1990563467', 'Depression\nersistent sadness, loss of interest, fatigue, changes in appetite, sleep disturbances.', 'cholostrol', 'xxxxx', '2024-06-29 03:13:46', '1988235678', '2024-06-28 18:11:50', 'pending'),
 	(9, 9, 12, '1990563467', ' Cough\n Persistent cough, throat irritation, chest discomfort.', 'no', 'wwww', '2024-06-29 03:14:29', '1988235678', '2024-06-28 18:13:06', 'Medication cancelled'),
-	(10, 10, 68, '1990563467', ' Skin Rash\nRedness, itching, swelling, blisters.', 'no', 'piriton', '2024-06-29 03:06:50', '1988235678', '2024-06-28 18:15:56', 'pending'),
+	(10, 10, 68, '1990563467', ' Skin Rash\nRedness, itching, swelling, blisters.', 'no', 'piriton', '2024-06-29 03:06:50', '1988235678', '2024-06-28 18:15:56', 'true'),
 	(11, 3, 45, '1990563467', 'cholostrol', 'no', 'Perryton', '2024-06-30 10:24:46', '1988235678', '2024-06-28 18:48:00', 'true'),
 	(12, 4, 56, '1990563467', 'cholostrol', 'yes', '', NULL, '0000000000', '2024-06-28 18:49:07', 'pending'),
 	(13, 5, 54, '1990563467', 'xxxx', 'no', '', NULL, '0000000000', '2024-06-29 03:12:43', 'pending'),
